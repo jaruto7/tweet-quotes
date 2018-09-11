10d9596f439df3803085818d4aed67ea0ed141b8
Jarek Kajko <jaruto7@gmail.com>
2018-09-11 19:58:35 +0200

    Fix twitter code in script.js - part 10

--- js/script.js
+++ js/script.js
@@ -32,13 +32,13 @@ function createTweet( input ) {
         document.querySelector( '.quote' ).innerText = quoteText;
         document.querySelector( '.author' ).innerText = 'Author: ' + quoteAuthor;
         document.querySelector( '.tweet' ).setAttribute( 'href', tweet );
+    
+        document.addEventListener( 'DOMContentLoaded', function() {
+            getQuote();
+            document.querySelector( '.trigger' ).addEventListener( 'click', function() {
+                console.log('test');
+                getQuote();
+            });
+        });
     }
 }
-
-document.addEventListener( 'DOMContentLoaded', function() {
-    getQuote();
-    document.querySelector( '.trigger' ).addEventListener( 'click', function() {
-        console.log('test');
-        getQuote();
-    });
-});
